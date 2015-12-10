9ffca0a4363cb7fd64ccfca6948ed9788ca929e4
Maxim Tishchenko <qwertmax@gmail.com>
2015-12-10 21:01:55 +0600

    fix wercker

--- s1.go
+++ s1.go
@@ -1,16 +1,16 @@
-package goproxy
+package main
 
-import (
-	"fmt"
-	"net/http"
-)
+// import (
+// 	"fmt"
+// 	"net/http"
+// )
 
-var port = 9091
+// var port = 9091
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "%d\n%s\n", port, "app1")
-	})
-	println("ready 9091")
-	http.ListenAndServe(":9091", nil)
-}
+// func main() {
+// 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+// 		fmt.Fprintf(w, "%d\n%s\n", port, "app1")
+// 	})
+// 	println("ready 9091")
+// 	http.ListenAndServe(":9091", nil)
+// }
--- s2.go
+++ s2.go
@@ -1,16 +1,16 @@
-package goproxy
+package main
 
-import (
-	"fmt"
-	"net/http"
-)
+// import (
+// 	"fmt"
+// 	"net/http"
+// )
 
-var port = 9092
+// var port = 9092
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "%d\n%s\n", port, "app2")
-	})
-	println("ready 9092")
-	http.ListenAndServe(":9092", nil)
-}
+// func main() {
+// 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+// 		fmt.Fprintf(w, "%d\n%s\n", port, "app2")
+// 	})
+// 	println("ready 9092")
+// 	http.ListenAndServe(":9092", nil)
+// }
